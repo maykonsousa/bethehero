ece65449420102bf7ae6c1c7bdb8669f1acc94f2
Maykon Sousa <maykon.sousa@hotmail.com>
2020-03-28 11:40:00 -0300

    configurando as validações

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {errors} from 'celebrate'
 import cors from 'cors'
 import routes from './routes'
 
@@ -6,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import {celebrate, Segments, Joi} from 'celebrate';
 import ongController from './controllers/ongController'
 import incidentController from './controllers/incidentController'
 import sessionController from './controllers/sessionController'
@@ -6,7 +7,15 @@ import profileController from './controllers/profileController'
 
 const routes = Router();
 routes.get('/ongs', ongController.index); // lista todas as ongs
-routes.post('/ongs', ongController.create);//cria ong
+routes.post('/ongs',celebrate({
+  [Segments.BODY]:Joi.object().keys({
+    name:Joi.string().required(),
+    email:Joi.string().required().email(),
+    whatsapp:Joi.number().required().min(10).max(11),
+    city:Joi.string().required(),
+    uf:Joi.string().required().length(2),
+  })
+}) ,ongController.create);//cria ong
 routes.post('/session',sessionController.create); //faz o login da ong
 routes.get('/incidents',incidentController.index)//lista todos os casos
 routes.post('/incidents',incidentController.create)//cria um caso
